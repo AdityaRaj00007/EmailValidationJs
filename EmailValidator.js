f8ba9e43eeee2b565de4050de0f282c9379ed7cb
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-05 14:38:06 +0530

    [Aditya] Add . UC-3 feature Ensure . after domain and validate the mandatory 3rd part i.e. co

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,11 +1,10 @@
-//regex to validate until .
-const regex = /^[a-zA-Z0-9]+@[a-zA-z]+/;
+//regex to validate email
+const regex = /^[a-zA-Z0-9]+@[a-zA-z]+\.[a-zA-Z]+/;
 
 //function to validate email
 function validate(email){
     return regex.test(email)
 }
-
-//test
 console.log(validate("abc"));
 console.log(validate("abc@bridgelabz"));
+console.log(validate("abc@bridgelabz.co"));
